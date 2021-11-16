4451af99185f9bab30cc1ece00bae4e3a29d03dc
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-11-16 17:25:58 +0100

    Create Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import RoutesTree from './Components/RoutesTree';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
